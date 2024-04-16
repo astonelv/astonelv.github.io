83d9f641e8ea2265b6b60a4cbbc4cbccbc3a8db8
astonelv <158239756+astonelv@users.noreply.github.com>
2024-04-15 18:47:50 -0700

    Word Array

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello')
+const inputs = document.querySelector(".inputs"),
+resetBtn = document.querySelector(".reset-btn"),
+hint = document.querySelector(".hint span");
+
+
+function randomWord () {
+    //getting random word from array
+    let ranObj = wordList [Math.floor(Math.random() * wordList.length)];
+    let word = ranObj.word; //getting word of random object
+    console.log(ranObj);
+
+    hint.innerText = ranObj.hint;
+
+    let html = "";
+    for (let i = 0; i < word.length; i++){
+        html += `<input type = "text" disabled>`;
+    }
+    inputs.innerHTML = html;
+}
+randomWord();
+
+resetBtn.addEventListener("click", randomWord);
